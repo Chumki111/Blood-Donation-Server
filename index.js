354058f0a2c72fb26581f30fb99a73bcf8ca89b3
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-09 21:12:36 +0600

    get service single api

--- index.js
+++ index.js
@@ -268,6 +268,14 @@ async function run() {
             res.send(result)
         })
 
+        // get single service
+        app.get('/service/:id',async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)}
+            const result = await serviceSection.findOne(query);
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
