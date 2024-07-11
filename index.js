eacf5eb39c385e13b4a1f545d8d88e118e84ba21
Chumki khatun <chumkikhatun370@gmail.com>
2024-07-11 13:44:13 +0600

    get single blog data

--- index.js
+++ index.js
@@ -282,7 +282,13 @@ async function run() {
         res.send(result)
     })
 
-    
+    // get single blog
+    app.get('/blog/:id',async(req,res) =>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await blogCollection.findOne(query);
+        res.send(result)
+    })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
